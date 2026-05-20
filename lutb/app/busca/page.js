b6acafa38191d49d449e47b0fdb43ab3e5448614
LuizHenriqueCamello <luiz.00000852839@unicap.br>
2026-05-20 11:00:00 +0000

    recuperação da busca

--- lutb/app/busca/page.js
+++ lutb/app/busca/page.js
@@ -1,8 +1,8 @@
 "use client";
-
+ 
 import { Suspense } from "react";
 import BuscaConteudo from "./BuscaConteudo";
-
+ 
 export default function Page() {
   return (
     <Suspense fallback={<div style={{ padding: "20px" }}>Carregando...</div>}>
